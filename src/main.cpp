55aeab616f295923a75257a953b28b339203b2dd
MuffinYYY <muffinsherlocknils@gmail.com>
2022-10-03 21:58:20 +0300

    Added tracking of current LED state

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WiFiMulti.h> 
-#include <ESP8266mDNS.h>
 #include <ESP8266WebServer.h>   // Include the WebServer library
 
 ESP8266WiFiMulti wifiMulti;     // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
@@ -16,11 +15,11 @@ const int output4 = 4;
 void handleON();              // function prototypes for HTTP handlers
 void handleOFF();
 void handleNotFound();
+void handleCurrent();
 
 void setup(void){
-  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
+  Serial.begin(115200);
   delay(10);
-  Serial.println('\n');
 
   pinMode(output4, OUTPUT);  
   digitalWrite(output4, LOW);
@@ -33,20 +32,14 @@ void setup(void){
     delay(250);
     Serial.print('.');
   }
-  Serial.println('\n');
   Serial.print("Connected to ");
   Serial.println(WiFi.SSID());              // Tell us what network we're connected to
   Serial.print("IP address:\t");
   Serial.println(WiFi.localIP());           // Send the IP address of the ESP8266 to the computer
 
-  if (MDNS.begin("esp8266")) {              // Start the mDNS responder for esp8266.local
-    Serial.println("mDNS responder started");
-  } else {
-    Serial.println("Error setting up MDNS responder!");
-  }
-
   server.on("/led/true", handleON);               // Call the 'handleRoot' function when a client requests URI "/"
   server.on("/led/false", handleOFF);
+  server.on("/led/current", handleCurrent);
   server.onNotFound(handleNotFound);        // When a client requests an unknown URI (i.e. something other than "/"), call function "handleNotFound"
 
   server.begin();                           // Actually start the server
@@ -57,19 +50,32 @@ void loop(void){
   server.handleClient();                    // Listen for HTTP requests from clients
 }
 
-//Handle all HTTP requests 
+//Handle LED on HTTP request 
 void handleON() {
   server.sendHeader("Access-Control-Allow-Origin", "*");
-  server.send(200, "text/json", "{\"name\": \"LED ON\"}");
+  server.send(200, "text/json", "{\"LED_state\": \"true\"}");
   digitalWrite(output4, HIGH);
 }
 
+//Handle LED off HTTP request 
 void handleOFF() {
   server.sendHeader("Access-Control-Allow-Origin", "*");
-  server.send(200, "text/json", "{\"name\": \"LED OFF\"}");
+  server.send(200, "text/json", "{\"LED_state\": \"false\"}");
   digitalWrite(output4, LOW);
 }
 
+//Handle current LED state HTTP request
+void handleCurrent() {
+  server.sendHeader("Access-Control-Allow-Origin", "*");
+  if(digitalRead(output4) == HIGH){
+    server.send(200, "text/json", "{\"LED_state\": \"true\"}");
+  }
+  if(digitalRead(output4) == LOW){
+    server.send(200, "text/json", "{\"LED_state\": \"false\"}");
+  }
+}
+
+//Handle HTTP request to link that doesn't exist
 void handleNotFound(){
   server.send(404, "text/plain", "404: Not found");
 }
